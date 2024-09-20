cde507a5c4b6a06631644abd8f9d098de4a93a6e
ZimmermannG <113886143+WhitePhantomD@users.noreply.github.com>
2024-09-20 12:21:25 +0200

    functions defaults

--- functions.js
+++ functions.js
@@ -7,12 +7,20 @@
 //     return a + b;
 // }
 
-const add = function(a, b, ...args) {
+// const add = function(a, b, ...args) {
+//     // function expression
+//     console.log(args);
+//     const value = a + b;
+//     return args.reduce((acc, val) => acc + val, value);
+// }
+
+// // console.log(add(1, 2));
+// console.log(add(1,2,3,4,5,6,7,8,9,10));
+
+const add = function(a = 0, b = 0) {
     // function expression
-    console.log(args);
-    const value = a + b;
-    return args.reduce((acc, val) => acc + val, value);
+    // return a + b||0;
+    return a + b;
 }
 
-// console.log(add(1, 2));
-console.log(add(1,2,3,4,5,6,7,8,9,10));
+console.log(add(2));
